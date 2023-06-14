12c82f106b206a3b8309cd1d3915ed96a6fbe386
Bronad <90397301+Bronad@users.noreply.github.com>
2023-06-14 16:22:51 +0200

    Finished TodoList

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -3,6 +3,13 @@ const list = [10,20,30];
     
 const todos1 = [];
 const todos2 = [];
+const todos3 = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+  }, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+  }];
         
 
 function addItem(){
@@ -24,7 +31,43 @@ function addItem2(){
     for(let i = 0;  i < todos2.length; i++){
         outputString = outputString +`<p>${todos2[i]}</p>`
     }
-    document.querySelector('.js-todooutput').innerHTML = outputString;
+    document.querySelector('.js-todooutput2').innerHTML = outputString;
     inputTodo1.value = '';
 
+}
+
+function addItem3(){
+
+    const inputTodo = document.querySelector('.js-todoinput3');
+    const inputString = inputTodo.value;
+
+    const dateInputElement = document.querySelector('.js-todoinputdate3');
+    const dateInputString = dateInputElement.value;
+
+    todos3.push({name: inputString, dueDate: dateInputString});
+    inputTodo.value = '';
+    renderTodoList();
+
+}
+
+function renderTodoList(){
+
+    let todoListHTML = '';
+
+    for (let i = 0; i < todos3.length; i++) {
+        const todoObject = todos3[i];
+        const {name, dueDate} = todoObject;
+        
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+          todos3.splice(${i}, 1);
+          renderTodoList();
+        " class="deletebutton">Delete</button>
+      `;
+      todoListHTML += html;
+    }
+  
+    document.querySelector('.js-todooutput3').innerHTML = todoListHTML;
 }
