f4d187b4d5dbc6edf49af1f794df29997e840ebb
Bronad <90397301+Bronad@users.noreply.github.com>
2023-07-01 22:29:41 +0200

    Class added to Hold products.

---
+++ Amazon/javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,21 @@
+// product = {
+//     image: '',
+//     name: '', 
+// }
+
+class product {
+    constructor(image, name, stars, count, price ) {
+        this.image = image;
+        this.name = name;
+        this.stars = stars;
+        this.count = count;
+        // saved in Cents
+        this.price = price;
+    }
+}
+
+products = [product('', )]
+
+function ItemCreater() {}
+
+console.log('hello');
